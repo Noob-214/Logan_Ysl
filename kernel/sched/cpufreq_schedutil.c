5e4e06f808fc1b32caf8ea99299821b6356e0314
Viresh Kumar <viresh.kumar@linaro.org>
2018-06-17 03:10:11 +0300

    cpufreq: schedutil: Don't set next_freq to UINT_MAX
    The schedutil driver sets sg_policy->next_freq to UINT_MAX on certain
    occasions to discard the cached value of next freq:
    - In sugov_start(), when the schedutil governor is started for a group
      of CPUs.
    - And whenever we need to force a freq update before rate-limit
      duration, which happens when:
      - there is an update in cpufreq policy limits.
      - Or when the utilization of DL scheduling class increases.
    
    In return, get_next_freq() doesn't return a cached next_freq value but
    recalculates the next frequency instead.
    
    But having special meaning for a particular value of frequency makes the
    code less readable and error prone. We recently fixed a bug where the
    UINT_MAX value was considered as valid frequency in
    sugov_update_single().
    
    All we need is a flag which can be used to discard the value of
    sg_policy->next_freq and we already have need_freq_update for that. Lets
    reuse it instead of setting next_freq to UINT_MAX.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com> - backported to 4.4
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -95,12 +95,6 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 	s64 delta_ns;
 
 	if (unlikely(sg_policy->need_freq_update)) {
-		sg_policy->need_freq_update = false;
-		/*
-		 * This happens when limits change, so forget the previous
-		 * next_freq value and force an update.
-		 */
-		sg_policy->next_freq = UINT_MAX;
 		return true;
 	}
 

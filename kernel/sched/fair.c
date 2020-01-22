5ab5208018c95f6b3ef767534fa909b12bc198a9
Ethan Halsall <ethan.halsall@gmail.com>
2020-01-22 10:15:47 +0000

    sched/fair: improve select_task_rq_fair coding style
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7768,8 +7768,9 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 	if (sd_flag & SD_BALANCE_WAKE) {
 		int _wake_cap = wake_cap(p, cpu, prev_cpu);
+		int _cpus_allowed = cpumask_test_cpu(cpu, tsk_cpus_allowed(p));
 
-		if (cpumask_test_cpu(cpu, tsk_cpus_allowed(p))) {
+		if (_cpus_allowed) {
 			bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 			if (sysctl_sched_sync_hint_enable && sync &&
@@ -7778,8 +7779,9 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 		}
 
 		record_wakee(p);
-		want_affine = !wake_wide(p, sibling_count_hint) && !_wake_cap
-			      && cpumask_test_cpu(cpu, &p->cpus_allowed);
+		want_affine = (!wake_wide(p, sibling_count_hint) && !_wake_cap
+			      && _cpus_allowed);
+
 	}
 
 	if (energy_aware()) {

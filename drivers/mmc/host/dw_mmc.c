34d04ee61ac56e6dedd2726d9f736b12a1446526
Sandeep Chahal <sandeeps567ingh@gmail.com>
2021-10-07 16:59:30 +0000

    Revert "Merge tag 'v4.9.281' into xr"
    This reverts commit e0811f4353291b810046049a69a9abd6307a1794, reversing
    changes made to 7d33b3cbddb954555244e4b17de9d0f0b7a8c7a2.

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -380,7 +380,7 @@ static void dw_mci_start_command(struct dw_mci *host,
 
 static inline void send_stop_abort(struct dw_mci *host, struct mmc_data *data)
 {
-	struct mmc_command *stop = &host->stop_abort;
+	struct mmc_command *stop = data->stop ? data->stop : &host->stop_abort;
 
 	dw_mci_start_command(host, stop, host->stop_cmdr);
 }
@@ -1280,7 +1280,10 @@ static void __dw_mci_start_request(struct dw_mci *host,
 		spin_unlock_irqrestore(&host->irq_lock, irqflags);
 	}
 
-	host->stop_cmdr = dw_mci_prep_stop_abort(host, cmd);
+	if (mrq->stop)
+		host->stop_cmdr = dw_mci_prepare_command(slot->mmc, mrq->stop);
+	else
+		host->stop_cmdr = dw_mci_prep_stop_abort(host, cmd);
 }
 
 static void dw_mci_start_request(struct dw_mci *host,
@@ -1866,8 +1869,8 @@ static void dw_mci_tasklet_func(unsigned long priv)
 					continue;
 				}
 
-				send_stop_abort(host, data);
 				dw_mci_stop_dma(host);
+				send_stop_abort(host, data);
 				state = STATE_SENDING_STOP;
 				break;
 			}
@@ -1891,10 +1894,11 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			 */
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
-				if (!(host->data_status & (SDMMC_INT_DRTO |
+				dw_mci_stop_dma(host);
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
 							   SDMMC_INT_EBE)))
 					send_stop_abort(host, data);
-				dw_mci_stop_dma(host);
 				state = STATE_DATA_ERROR;
 				break;
 			}
@@ -1927,10 +1931,11 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			 */
 			if (test_and_clear_bit(EVENT_DATA_ERROR,
 					       &host->pending_events)) {
-				if (!(host->data_status & (SDMMC_INT_DRTO |
+				dw_mci_stop_dma(host);
+				if (data->stop ||
+				    !(host->data_status & (SDMMC_INT_DRTO |
 							   SDMMC_INT_EBE)))
 					send_stop_abort(host, data);
-				dw_mci_stop_dma(host);
 				state = STATE_DATA_ERROR;
 				break;
 			}
@@ -2004,7 +2009,7 @@ static void dw_mci_tasklet_func(unsigned long priv)
 			host->cmd = NULL;
 			host->data = NULL;
 
-			if (!mrq->sbc && mrq->stop)
+			if (mrq->stop)
 				dw_mci_command_complete(host, mrq->stop);
 			else
 				host->cmd_status = 0;
--- fs/namespace.c
+++ fs/namespace.c
@@ -1687,22 +1687,13 @@ static inline bool may_mount(void)
 	return ns_capable(current->nsproxy->mnt_ns->user_ns, CAP_SYS_ADMIN);
 }
 
-#ifdef	CONFIG_MANDATORY_FILE_LOCKING
-static bool may_mandlock(void)
-{
-	pr_warn_once("======================================================\n"
-		     "WARNING: the mand mount option is being deprecated and\n"
-		     "         will be removed in v5.15!\n"
-		     "======================================================\n");
-	return capable(CAP_SYS_ADMIN);
-}
-#else
 static inline bool may_mandlock(void)
 {
-	pr_warn("VFS: \"mand\" mount option not supported");
+#ifndef	CONFIG_MANDATORY_FILE_LOCKING
 	return false;
-}
 #endif
+	return capable(CAP_SYS_ADMIN);
+}
 
 /*
  * Now umount can handle mount points as well as block devices.
